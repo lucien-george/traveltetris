93359f5e6161bbef883a7914de7ddd66f9f457d7
Lucien George <luciengeorge95@gmail.com>
2019-05-28 18:15:36 +0100

    done

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -17,24 +17,38 @@ class PlansController < ApplicationController
 	def edit
 		@plan = Plan.find(params[:id])
 		@plan.plan_cities.build().build_city
+		@trip_date = TripDate.new
 	end
 
 	def update
 		@plan = Plan.find(params[:id])
-		@plan.update(plan_params)
-		# @cities = params[:plan][:plan_city_ids].reject(&:blank?).map { |id| City.find(id) }
-		# @plan.cities = @cities
+		if params[:plan]
+			@cities = params[:plan][:city_ids].reject(&:blank?).map { |id| City.find(id) }
+			@plan.cities = @cities
+		end
+		if params[:trip_dates]
+			start_dates = params[:trip_dates][:start_dates]
+			end_dates = params[:trip_dates][:end_dates]
+			start_dates.count.times do |c|
+				departure_date = start_dates[c].values.map { |v| v.rjust(2,'0')  }.join
+				arrival_date = end_dates[c].values.map { |v| v.rjust(2,'0')  }.join
+				TripDate.create(plan: @plan, departure_date: departure_date, arrival_date: arrival_date)
+			end
+		end
 		if @plan.save
-			redirect_to plan_path(@plan)
+			if @plan.cities.any? && @plan.trip_dates.any?
+				redirect_to plan_path(@plan)
+			else
+				redirect_to edit_plan_path(@plan)
+			end
 		else
 			render :edit
 		end
-		raise
 	end
 
 	private
 
 	def plan_params
-		params.require(:plan).permit(:city_id, plan_cities_attributes: [:id, :name])
+		params.require(:plan).permit(:city_id)
 	end
 end
--- app/controllers/trip_dates_controller.rb
+++ app/controllers/trip_dates_controller.rb
@@ -1,2 +1,24 @@
 class TripDatesController < ApplicationController
+	def new
+		@plan = Plan.find(params[:plan_id])
+		@trip_date = TripDate.new
+	end
+
+	def create
+		@plan = Plan.find(params[:plan_id])
+		@trip_date = TripDate.new(trip_dates_params)
+		@trip_date.plan = @plan
+		if @trip_date.save
+			redirect_to plan_path(@plan)
+		else
+			render :new
+		end
+		raise
+	end
+
+	private
+
+	def trip_dates_params
+		params.require(:trip_date).permit(:arrival_date, :departure_date)
+	end
 end
